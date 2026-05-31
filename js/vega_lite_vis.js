951367a7fdde38cfdcdd4f89503c8d695672e805
uwehz <jackylu0506@gmail.com>
2026-05-31 22:35:15 +1000

    FIT2179 Assignment 2

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,39 @@
+vegaEmbed("#chart_age_gender", "charts/chart_age_gender.vg.json")
+  .then(result => console.log("chart_age_gender loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_map", "charts/chart_map.vg.json")
+  .then(result => console.log("chart_map loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_heatmap", "charts/chart_heatmap.vg.json")
+  .then(result => console.log("chart_heatmap loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_bump", "charts/chart_bump.vg.json")
+  .then(result => console.log("chart_bump loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_organised", "charts/chart_organised.vg.json")
+  .then(result => console.log("chart_organised loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_gap", "charts/chart_gap.vg.json")
+  .then(result => console.log("chart_gap loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_diverging", "charts/chart_diverging.vg.json")
+  .then(result => console.log("chart_diverging loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_trend", "charts/chart_trend.vg.json")
+  .then(result => console.log("chart_trend loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_barriers", "charts/chart_barriers.vg.json")
+  .then(result => console.log("chart_barriers loaded"))
+  .catch(console.error);
+
+vegaEmbed("#chart_motivations", "charts/chart_motivations.vg.json")
+  .then(result => console.log("chart_motivations loaded"))
+  .catch(console.error);
